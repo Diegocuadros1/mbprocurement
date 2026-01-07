1a5689988f4821d2ca4c30f5ed0e8a284d94b54b
Diego Cuadros <cuadrosda21@gmail.com>
2026-01-06 22:03:35 -0800

    finalized ordering demo part 1

---
+++ TODO.md
@@ -0,0 +1,1 @@
+Admin should also have an option to add an order for a company, and also delete an order that was requested.
--- app/dashboard/orders/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const page = () => {
-  return <div className="flex justify-center items-center">page</div>;
-};
